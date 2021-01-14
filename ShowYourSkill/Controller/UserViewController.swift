172e816a38be18d7ebae48622ae63c85fc047ca6
Boubakar <t.boubakar@outlook.com>
2021-01-14 14:32:36 +0100

    Premier commit, Création des vues

---
+++ ShowYourSkill/Controller/UserViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ShowYourSkill
+//
+//  Created by Boubakar Traore on 14/01/2021.
+//  Copyright © 2021 Boubakar Traore. All rights reserved.
+//
+
+import UIKit
+
+class UserViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
